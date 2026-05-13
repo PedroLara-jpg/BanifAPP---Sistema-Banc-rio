3968a6f0f51cb97aea08d599bb4fd369aedbe0cd
Pedro-jpg <mrpedro342@gmail.com>
2026-05-13 07:53:28 -0300

    Limpeza de Scaffolds.

--- backend/app/controllers/new_account_controller.ts
+++
@@ -1,8 +0,0 @@
-// This controller is not used in BANIF. Client registration is handled by ClientsController.
-import type { HttpContext } from '@adonisjs/core/http'
-
-export default class NewAccountController {
-  async store({ response }: HttpContext) {
-    return response.notFound({ message: 'Use POST /clients para cadastrar clientes.' })
-  }
-}
